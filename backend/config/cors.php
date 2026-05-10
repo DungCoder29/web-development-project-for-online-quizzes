e5596081c990fed633ed334942e830abd61f4e10
DungCoder29 <quangdunglh2003@gmail.com>
2026-05-11 06:03:49 +0700

    fix CORS for Vercel

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,10 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173'],
+    'allowed_origins' => [
+    'http://localhost:5173',
+    'https://web-development-project-for-online.vercel.app',
+    ],
 
     'allowed_origins_patterns' => [],
 
